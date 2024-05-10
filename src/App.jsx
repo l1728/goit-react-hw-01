4d5955e6065437343ff7d3cd8dd9ce52865644b1
Tetyana Gogulya <cheburashka077@ukr.net>
2024-05-10 23:13:01 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+const App = () => {
+  return (
+    <>
+      {/* <Profile {...userData} /> */}
+      {/* <FriendList friends={friends} /> */}
+      {/* <TransactionHistory items={transactions} /> */}
+    </>
+  );
+};
+export default App;
