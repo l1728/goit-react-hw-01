8030f747aefc86d27264b1a4d8034ee0fde2fa26
Tetyana Gogulya <cheburashka077@ukr.net>
2024-05-11 01:23:06 +0300

    Proile_jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import Profile from './Profile/Profile';
+import userData from '../userData.json';
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      {/* <FriendList friends={friends} /> */}
+      {/* <TransactionHistory items={transactions} /> */}
+    </>
+  );
+};
+export default App;
