4ee12ae582dcb34ebfe7bbd60bf89c6f18894ec9
Tetyana Gogulya <cheburashka077@ukr.net>
2024-05-11 17:48:12 +0300

    trans_css

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -3,20 +3,20 @@ import styles from '../TransactionHistory/TransactionHistory.module.css';
 const TransactionHistory = ({ items }) => {
   return (
     <table className={styles.table}>
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+      <thead className={styles.thead}>
+        <tr className={styles.columnHead}>
+          <th className={styles.columnCapt}>Type</th>
+          <th className={styles.columnCapt}>Amount</th>
+          <th className={styles.columnCapt}>Currency</th>
         </tr>
       </thead>
 
       <tbody>
         {items.map(transaction => (
-          <tr key={transaction.id}>
-            <td>{transaction.type}</td>
-            <td>{transaction.amount}</td>
-            <td>{transaction.currency}</td>
+          <tr className={styles.columnBody} key={transaction.id}>
+            <td className={styles.columnBodyType}>{transaction.type}</td>
+            <td className={styles.columnBodyAmCur}>{transaction.amount}</td>
+            <td className={styles.columnBodyAmCur}>{transaction.currency}</td>
           </tr>
         ))}
       </tbody>
