39cf187e0df7a07c19ba9adb63b99fc7cc3a4762
Tetyana Gogulya <cheburashka077@ukr.net>
2024-05-11 15:46:00 +0300

    css_profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,27 +1,29 @@
+import styles from '../Profile/Profile.module.css';
+
 const Profile = ({ name, tag, location, image, stats }) => {
   const { followers, views, likes } = stats;
 
   return (
-    <div>
-      <div>
-        <img src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+    <div className={styles.container}>
+      <div className={styles.caption}>
+        <img className={styles.photo} src={image} alt="User avatar" />
+        <p className={styles.profName}>{name}</p>
+        <p className={styles.profTagLoc}>@{tag}</p>
+        <p className={styles.profTagLoc}>{location}</p>
       </div>
 
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>{followers}</span>
+      <ul className={styles.profInfo}>
+        <li className={styles.profActiv}>
+          <span className={styles.activCapt}>Followers</span>
+          <span className={styles.activNumb}>{followers}</span>
         </li>
-        <li>
-          <span>Views</span>
-          <span>{views}</span>
+        <li className={styles.profActiv}>
+          <span className={styles.activCapt}>Views</span>
+          <span className={styles.activNumb}>{views}</span>
         </li>
-        <li>
-          <span>Likes</span>
-          <span>{likes}</span>
+        <li className={styles.profActiv}>
+          <span className={styles.activCapt}>Likes</span>
+          <span className={styles.activNumb}>{likes}</span>
         </li>
       </ul>
     </div>
